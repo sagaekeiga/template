a9177918c20925ac14c9ecefb1970d59529403cb
Keiga Sagae <sagae5.28rujeae@gmail.com>
2017-12-28 01:50:42 +0900

    remote

--- template.rb
+++ template.rb
@@ -259,7 +259,7 @@ run 'echo "# #{@app_name}" >> README.md'
 git :init
 git add: "."
 git commit: %Q{ -m 'Initial commit' }
-git remote: "add origin ssh://github.com/sagaekeiga/#{app_name}.git"
+git remote: "add origin #{app_name}"
 git push: '-u origin master'
 
 
