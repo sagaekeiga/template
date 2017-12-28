b22818683bb7fde89201787ad652c845505666e2
Keiga Sagae <sagae5.28rujeae@gmail.com>
2017-12-28 11:57:19 +0900

    rubocop

--- template.rb
+++ template.rb
@@ -115,7 +115,7 @@ gem_group :development, :test do
   gem 'database_rewinder'
   gem 'json_spec'
   gem 'guard-rspec'
-  gem 'rubocop'
+  gem 'rubocop', '~> 0.49.0'
   gem 'letter_opener_web'
   gem 'simplecov', require: false
   gem 'webmock'
