649d9fade6174849f38d1ebcb39eeb8b35d1b769
Keiga Sagae <sagae5.28rujeae@gmail.com>
2018-01-07 20:39:50 +0900

    migrate

--- template.rb
+++ template.rb
@@ -436,51 +436,6 @@ CODE
 #
 run 'rails g devise:install'
 remove_file 'config/environments/development.rb'
-file 'config/environments/development.rb', <<-CODE
-Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
-
-  # mailer
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
-  # Do not eager load code on boot.
-  config.eager_load = false
-
-  # Show full error reports.
-  config.consider_all_requests_local = true
-
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
-
-  # Print deprecation notices to the Rails logger.
-  config.active_support.deprecation = :log
-
-  # Raise an error on page load if there are pending migrations.
-  config.active_record.migration_error = :page_load
-
-  # Debug mode disables concatenation and preprocessing of assets.
-  # This option may cause significant delays in view rendering with a large
-  # number of complex assets.
-  config.assets.debug = true
-
-  # Suppress logger output for asset requests.
-  config.assets.quiet = true
-
-  # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
-
-  # Use an evented file watcher to asynchronously detect changes in source code,
-  # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-end
-CODE
 run 'rails g devise:views'
 run 'rails g devise User'
 
